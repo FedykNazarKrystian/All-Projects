3b7f6f2d760ef81444308aeb74e867115f7b1f36
Fedyk Nazar Krystian <fedyk.nazar.krystian@gmail.com>
2024-08-27 11:54:02 -0400

    Updated Sensor Monitoring

--- sensor_monitoring/sensor_monitoring/main_app/views.py
+++ sensor_monitoring/sensor_monitoring/main_app/views.py
@@ -1,3 +1,20 @@
+
+import csv
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import SensorReading, Sensor
+
+def export_reading_csv(request):
+    response = HttpResponse(content_type="text/csv")
+    response['Content-Disposition'] = "attachment; filename='reading.csv'"
+    writer = csv.writer(response)
+    writer.writerow(['Sensor', 'Times_Temp', 'Value'])
+    readings = SensorReading.objects.all().value_list('sensor_name', 'times_temp', 'value')
+    for r in readings:
+        writer.writerow(r)
+        return response
+
+def index(request):
+
+    return render(request, '', {'sensor': sensor})
